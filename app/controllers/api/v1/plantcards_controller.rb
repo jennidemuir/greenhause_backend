451859e8bea06f0eb1d293c773388a53e9857145
stellawobble <jennidemuir@gmail.com>
2020-07-21 13:20:00 -0700

    update controllers

--- app/controllers/api/v1/plantcards_controller.rb
+++ app/controllers/api/v1/plantcards_controller.rb
@@ -1,2 +1,38 @@
 class Api::V1::PlantcardsController < ApplicationController
+
+    def index
+        plantcard = Plantcard.select {|s| s.user_id == current_user.id}
+       render json: plantcard, only: [:id, :commonname, :scientificname, :description]
+
+    end
+   def show
+       plantcard = Plantcard.find_by(id: params[:id])
+       if plantcard
+           render json: plantcard.slice(:id, :commonname, :scientificname, :description)
+       else 
+           render json: {message: "Plant Card Not Found!"}
+       end
+   end
+
+   def new
+       plantcard = Plantcard.now
+    end
+
+   def create
+        plantcard = Plantcard.create(plantcard_params)
+        render json: {message: "success"}
+    end
+    
+    def destroy
+        plantcard = Plantcard.find_by(id: params[:id])
+        plantcard.destroy()
+        render json: {message: "Plant Card Has Been Deleted!"}
+    end
+
+private
+def plantcard_params
+    params.require(:plantcard).permit(:commonname, :scientificname, :description)
+end
+
+
 end
--- app/controllers/api/v1/plantcollections_controller.rb
+++ app/controllers/api/v1/plantcollections_controller.rb
@@ -1,2 +1,37 @@
 class Api::V1::PlantcollectionsController < ApplicationController
+    
+    def index
+        plantcollection = plantcollection.select {|s| s.user_id == current_user.id}
+       render json: plantcollection, only: [:id, :user_id, :plantcard_id]
+
+    end
+   def show
+       plantcollection = Plantcollection.find_by(id: params[:id])
+       if plantcollection
+           render json: plantcollection.slice(:id, :user_id, :plantcard_id)
+       else 
+           render json: {message: "Plant Collection Not Found!"}
+       end
+   end
+
+   def new
+       plantcollection = Plantcollection.now
+    end
+
+   def create
+        plantcollection = Plantcollection.create(plantcollection_params)
+        render json: {message: "success"}
+    end
+    
+    def destroy
+        plantcollection = Plantcollection.find_by(id: params[:id])
+        plantcollection.destroy()
+        render json: {message: "Plant Collection Has Been Deleted!"}
+    end
+
+private
+def plantcollection_params
+    params.require(:plantcollection).permit(:user_id, :plantcard_id)
+end
+
 end
